ca2419a425c55323d72b43ab95ae66a6064f00b5
Preet Mehta <mehtapreet404@gmail.com>
2025-05-08 17:25:40 +0530

    Updated code to use env variables

--- src/lib/sequelize.js
+++ src/lib/sequelize.js
@@ -2,11 +2,11 @@ import { Sequelize } from "sequelize";
 import pg from "pg";
 
 export const sequelize = new Sequelize(
-  "sprinto_learning",
-  process.env.POSTGRES_USERNAME,
+  process.env.POSTGRES_DATABASE,
+  process.env.POSTGRES_USER,
   process.env.POSTGRES_PASSWORD,
   {
-    host: "localhost",
+    host: POSTGRES_HOST,
     dialect: "postgres",
     dialectModule: pg,
   }
