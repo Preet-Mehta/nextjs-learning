fcc3b9590b6ce5cd0455c45adcf58f52b79c5f14
Preet Mehta <mehtapreet404@gmail.com>
2025-05-08 18:08:44 +0530

    Used env variable for setting apollo server

--- src/components/ApolloClient.js
+++ src/components/ApolloClient.js
@@ -3,7 +3,7 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:3000/api/graphql",
+  uri: `http://${process.env.VERCEL_URL}/api/graphql`,
   cache: new InMemoryCache(),
 });
 
